37dfa6dfbc61095bc41bdbb924e5005f23ebc7e9
daoleduyhung <daoleduyhung04@gmail.com>
2020-04-17 17:53:44 +0700

    Update duyetdetai.blade.php

--- resources/views/pages/duyetdetai.blade.php
+++ resources/views/pages/duyetdetai.blade.php
@@ -8,11 +8,14 @@
 {{-- <h1 class="h3 mb-2 text-gray-800">Tables</h1>
 <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p> --}}
 
+
+
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Danh sách đề tài</h6>
+        <h6 class="m-0 font-weight-bold text-primary">Đề tài đợi duyệt</h6>
     </div>
+
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -27,18 +30,19 @@
                 </tr>
                 </thead>
                     <tbody>
-                    @foreach ($danhsachdt as $dt)
+                    @foreach ($duyet as $dt)
                         @if($dt->count() > 0)
-                        <tr>
-                            <th scope="row">{{$stt++}}</th>
-                            <td>{{$dt->ho}} {{$dt->ten}}</td>
-                            <td>{{$dt->tendetai}}</td>
-                            <td>{{$dt->mota}}</td>
-                            <td>{{$dt->gvhd}}</td>
-                            <td><a href="javascript:" data-id="{{$dt->id}}" data-name="{{$dt->daduyet}}"  class="badge badge-success edit-btn">Duyệt</a>
-                            <a href="javascript:" class="badge badge-danger delete-btn" data-id="{{$dt->id}}">Xóa</a>
-                            </td>
-                        </tr>
+                            <tr>
+                                <th scope="row">{{$stt++}}</th>
+                                <td>{{$dt->ho}} {{$dt->ten}}</td>
+                                <td>{{$dt->tendetai}}</td>
+                                <td>{{$dt->mota}}</td>
+                                <td>{{$dt->gvhd}}</td>
+                                <td>
+                                    <a href="javascript:" data-id="{{$dt->id}}" class="btn btn-split btn-success form-group duyet-btn">Duyệt</a>
+                                    <a href="javascript:" class="btn btn-split btn-danger form-group delete-btn" data-id="{{$dt->id}}">Xóa</a>
+                                </td>
+                            </tr>
                         @endif
                     @endforeach
                 </tbody>
@@ -52,5 +56,28 @@
 
 </div>
 <!-- End of Main Content -->
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('.duyet-btn').click(function(){
+			id = $(this).data('id');
+            $.post('{{route('duyetdt')}}',{id:id,_token:"{{csrf_token()}}"}).done(function(){
+                location.reload();
+            }).fail(function(){
+                alert('Không thể hoàn thành thao tác này');
+            })
+            
+        })
+        $('.delete-btn').click(function(){
+			id = $(this).data('id');
+			if (confirm("Dữ liệu xoá sẽ không khôi phục được. Bạn có thật sự muốn xoá?")) {
+				$.post('{{route('delduyetdt')}}',{id:id,_token:"{{csrf_token()}}"}).done(function(){
+					location.reload();
+				}).fail(function(){
+					alert('Không thể hoàn thành thao tác này');
+				})
+            }
+		})
+	})
+</script>
 
 @endsection
