606f1f8fd6677711807f98cff995353c37a91529
daoleduyhung <daoleduyhung04@gmail.com>
2020-04-17 17:53:49 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::post('dangky','dangkycontroller@Register')->name('register');
 
 Route::get('addadmin','dangkycontroller@getaddadmin')->name('getaddadmin')->middleware('isadmin');
 Route::post('themadmin','dangkycontroller@addadmin')->name('addadmin');
+Route::post('edit','homeController@editUser')->name('editUser');
+Route::post('del','homeController@delUser')->name('delUser');
 
 Route::get('logout','dangnhapcontroller@logout')->name('logout');
 
@@ -37,12 +39,16 @@ Route::get('danhsachdetai','homecontroller@alldt')->name('dsdetai');
 
 Route::get('duyet','homecontroller@getduyetdt')->name('getduyetdt')->middleware('isadmin');
 Route::post('duyetdt','homecontroller@duyetdt')->name('duyetdt');
+Route::post('delduyetdt','homecontroller@duyetdt')->name('delduyetdt');
 
 Route::get('dangkydetai','homecontroller@getdkdetai')->name('getdkdetai');
 Route::post('xulydangkydetai','homecontroller@dkdetai')->name('dkdetai');
 
 Route::get('data','DataController@defaultdata')->name('data');
 
-Route::post('edit','homeController@editUser')->name('editUser');
-
-Route::post('del','homeController@delUser')->name('delUser');
+Route::group(['prefix'=>'thamkhao'],function(){
+    Route::get('danhsach','homeController@thamkhao')->name('thamkhao');
+    Route::post('add','homeController@addThamkhao')->name('addthamkhao');
+    Route::post('edit','homeController@editThamkhao')->name('editthamkhao');
+    Route::post('del','homeController@delThamkhao')->name('delthamkhao');
+});
