d03c17243321e626ea52fa8dd49cd5db43cb3536
daoleduyhung <daoleduyhung04@gmail.com>
2020-06-10 14:29:21 +0700

    Update 2020_02_12_041331_create__users_table.php

--- database/migrations/2020_02_12_041331_create__users_table.php
+++ database/migrations/2020_02_12_041331_create__users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('email',191)->unique();
-            $table->integer('level')->nullable();
+            $table->tinyinteger('level')->nullable();
             $table->text('img')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');        
